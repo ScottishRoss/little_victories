b68c5eedc33ecd094a78e2181c82a379fab446c1
Ross Henderson <ross.scottish@gmail.com>
2023-02-02 17:37:49 +0000

    Removed twitter and updated socialshare

--- lib/api/twitter_api.dart
+++
@@ -1,6 +0,0 @@
-class TwitterApi {
-  static const String apiKey = '<Your API-KEY>';
-  static const String apiKeySecret = '<Your SECRET-KEY here>';
-  static const String bearerToken = '<Your BEARER-TOKEN here>';
-  static const String redirectUrl = '<Your CALLBACK-URL here>';
-}
