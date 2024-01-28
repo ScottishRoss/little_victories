f2b403afb5d36e14afe50e94b79a269dab323f26
Ross Henderson <ross.henderson@chetwood.co>
2024-01-28 20:52:11 +0000

    refactor

--- lib/screens/sign_in/widgets/sign_in_widget.dart
+++ lib/screens/sign_in/widgets/sign_in_widget.dart
@@ -4,7 +4,6 @@ import 'package:little_victories/screens/sign_in/widgets/tag_line.dart';
 import 'package:little_victories/util/authentication.dart';
 import 'package:little_victories/widgets/common/google_sign_in_button.dart';
 import 'package:little_victories/widgets/common/lv_logo.dart';
-import 'package:little_victories/widgets/common/no_internet_connection.dart';
 
 class SignInWidget extends StatelessWidget {
   const SignInWidget({Key? key}) : super(key: key);
@@ -16,7 +15,9 @@ class SignInWidget extends StatelessWidget {
         builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
-              return const NoInternetConnection();
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
             case ConnectionState.waiting:
               return const Center(
                 child: CircularProgressIndicator(),
@@ -40,7 +41,6 @@ class SignInWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                // Little Victories Logo
                 LVLogo(),
                 TagLine(),
                 GoogleSignInButton(),
